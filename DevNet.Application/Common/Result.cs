847af6b5fc0b2824f73ce2e1e269149975eaac95
guiller santos <guillersantos111@gmail.com>
2025-11-15 14:56:51 +0800

    Created Result Class

--- DevNet.Application/Common/Result.cs
+++ DevNet.Application/Common/Result.cs
@@ -1,6 +1,35 @@
 ﻿namespace DevNet.Application.Common
 {
-    public class Result
+    public class Result<T>
     {
+        #region Properties
+
+        public bool IsSuccess { get; set; }
+        public string? Error { get; set; }
+        public T? Value { get; set; }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public static Result<T> Success(T value)
+        {
+            return new()
+            {
+                IsSuccess = true,
+                Value = value
+            };
+        }
+
+        public static Result<T> Failure(string error)
+        {
+            return new()
+            {
+                IsSuccess = false,
+                Error = error
+            };
+        }
+
+        #endregion Public Methods
     }
 }
